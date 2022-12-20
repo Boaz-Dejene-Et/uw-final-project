ea34fa77db0142b72edfb5133fc17cf69b083562
Boaz-Dejene-Et <boazdeju@gmail.com>
2022-12-20 11:03:12 +0300

    scroll trigger sample animation

--- js/shared/workWithUs.js
+++ js/shared/workWithUs.js
@@ -1,3 +1,17 @@
-tl.to('.work_with_us_image', 2,{
-    height: '100%',
-})
+let workWithUsImage = document.querySelector('.work_with_us_container')
+
+ScrollTrigger.create({
+    start: 0,
+    end: "max",
+    onUpdate: updateValues
+  });
+
+function updateValues() {
+    if(ScrollTrigger.isInViewport(workWithUsImage)) {
+        tl.to('.work_with_us_image', 2,{
+            height: '100%',
+        })
+    }
+}
+
+updateValues()
