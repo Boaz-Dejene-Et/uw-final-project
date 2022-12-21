9ca8f9db429412ff2ed697ee24bb8b03472f5828
Boaz-Dejene-Et <boazdeju@gmail.com>
2022-12-21 06:23:56 +0300

    update on the modal

--- js/main.js
+++ js/main.js
@@ -5,12 +5,17 @@ let tl = new TimelineLite();
 
 const closeModal = () => {
     modal.classList.add('close_modal')
+    modal.classList.remove('modal_container')
 }
 
 const showModal = () => {
     if(!seenModal) {
         modal.classList.add('modal_container')
+        modal.classList.remove('close_modal')
         localStorage.setItem("seenModal", true)
+        setTimeout(()=>{
+            localStorage.removeItem("seenModal")
+        }, 10000)
     }
 }
 
